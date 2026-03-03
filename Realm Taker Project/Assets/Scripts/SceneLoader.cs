1858643feccf1587b8a350c429ee98cfab179185
Jonathan <jonathanharyono4@gmail.com>
2026-03-03 23:00:59 +0700

    Added more stuff

--- Realm Taker Project/Assets/Scripts/SceneLoader.cs
+++ Realm Taker Project/Assets/Scripts/SceneLoader.cs
@@ -1,21 +1,44 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class SceneLoader: MonoBehaviour
 {
     [SerializeField] private string SceneToPlay;
     [SerializeField] private bool PressAnything;
     [SerializeField] private bool ImmediatelyLoad = false;
+    
+    public Animator transition;
+    public float transitionTime = 1.3f;
+    public bool animated = false;
 
 
     void Update()
     {
 
-        if ((PressAnything && Input.anyKeyDown) || ImmediatelyLoad)
+        if ((PressAnything && Keyboard.current.anyKey.wasPressedThisFrame) || ImmediatelyLoad)
         {
-            ChangeScene();
+            if (animated)
+            {
+                StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+            }
+            else
+            {
+                ChangeScene();
+            }
         }
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+        // Wait for the animation to finish
+        yield return new WaitForSeconds(transitionTime);
+        // Load the next scene
+        SceneManager.LoadScene(levelIndex);
     }
     public void ChangeScene()
     {
